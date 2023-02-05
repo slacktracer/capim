b2965f19dcd9a9e94431ed76b05590dd4631ce7e
slacktracer <slacktracer@gmail.com>
2023-02-04 23:43:07 -0300

    I think this was here by mistake...

--- main/components/Main.vue
+++ main/components/Main.vue
@@ -1,13 +1,3 @@
-<script lang="ts" setup>
-import { useStore } from "../stores/main-store.js";
-
-const store = useStore();
-
-const getAccounts = async () => {
-  await store.getAccounts();
-};
-</script>
-
 <template>
   <div>
     <h1>Capim</h1>
