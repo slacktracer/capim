a2f8dd4438361782df62a4631af27bce6cec8b68
slacktracer <slacktracer@gmail.com>
2023-02-26 19:22:11 -0300

    Displaying "boot errors" on the main screen...

--- main/components/Main.vue
+++ main/components/Main.vue
@@ -2,11 +2,16 @@
 import { useAccountsStore } from "../stores/accounts/use-accounts-store.js";
 
 const accountsStore = useAccountsStore();
+
+const { accounts } = accountsStore.state;
 </script>
 
 <template>
   <div>
     <h1>Capim</h1>
-    <div v-if="accountsStore.state.accounts.loading">Loading accounts...</div>
+
+    <div v-if="accounts.loading">Loading accounts...</div>
+
+    <div v-if="accounts.error">{{ accounts.error.message }}</div>
   </div>
 </template>
