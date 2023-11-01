26bdd7e3457f61a61b512b7d54923f3794c130d7
slacktracer <slacktracer@gmail.com>
2023-11-01 14:05:21 -0300

    Improving injectState.

--- main/modules/common/utils/inject-state.ts
+++ main/modules/common/utils/inject-state.ts
@@ -3,4 +3,4 @@ import type { InjectState } from "../../../types/InjectState.js";
 export const injectState: InjectState =
   (fun, state) =>
   (input, ...rest) =>
-    fun(Object.assign(input, { state }), ...rest);
+    fun(Object.assign(input ?? {}, { state }), ...rest);
--- main/types/InjectState.ts
+++ main/types/InjectState.ts
@@ -2,6 +2,6 @@ export type InjectState = <Function extends (...args: any[]) => any, State>(
   fun: Function,
   state: State,
 ) => (
-  input: Omit<Parameters<Function>[0], "state">,
+  input?: Omit<Parameters<Function>[0], "state">,
   ...rest: unknown[]
 ) => ReturnType<Function>;
