8f34af2c959138affd21c8e897d99e178f103f62
slacktracer <slacktracer@gmail.com>
2023-12-20 20:42:06 -0300

    Setting the initial amount when "making empty operations".

--- main/modules/operations/make-empty-operation.ts
+++ main/modules/operations/make-empty-operation.ts
@@ -1,6 +1,7 @@
 import type { Operation } from "../../core/types/Operation.js";
 
 export const makeEmptyOperation: () => Partial<Operation> = () => ({
+  amount: 1,
   amountPerUnit: 1,
   at: new Date().toISOString(),
   atTimezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
