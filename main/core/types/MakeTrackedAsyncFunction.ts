3de1eafc60e037f5de028f1cf84ab2586a431020
slacktracer <slacktracer@gmail.com>
2023-12-24 15:47:59 -0300

    Removing obsolete files! \o/

--- main/core/types/MakeTrackedAsyncFunction.ts
+++
@@ -1,6 +0,0 @@
-import type { TrackedAsyncFunctionState } from "./TrackedAsyncFunctionState";
-
-export type MakeTrackedAsyncFunction = <Input, Output>(input: {
-  asyncFunction: (input: Input) => Promise<Output>;
-  state: TrackedAsyncFunctionState<Output>;
-}) => (input: Input) => void;
--- main/core/types/MakeTrackedAsyncFunctionState.ts
+++
@@ -1,6 +0,0 @@
-import type { TrackedAsyncFunctionState } from "./TrackedAsyncFunctionState";
-
-export type MakeTrackedAsyncFunctionState = <Data>(input?: {
-  data: Data | Partial<Data> | undefined;
-  ready?: boolean;
-}) => TrackedAsyncFunctionState<Data>;
