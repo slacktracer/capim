f636f051600169290baf2b525e76890f2d6e9717
slacktracer <slacktracer@gmail.com>
2024-07-04 17:59:18 -0300

    Actually, I don't want nor I need any of that. ¯\_(ツ)_/¯

--- main/components/operations/OperationListItem.vue
+++ main/components/operations/OperationListItem.vue
@@ -1,8 +1,6 @@
 <script lang="ts" setup>
 import { format } from "date-fns";
-import { onMounted, ref } from "vue";
 
-import { useSwipeOperation } from "../../composables/use-swipe-operation";
 import type { Operation } from "../../core/types/Operation.js";
 import { useAccountsStore } from "../../modules/accounts/use-accounts-store.js";
 
@@ -11,29 +9,11 @@ const props = defineProps<{
 }>();
 
 const accountStore = useAccountsStore();
-
-const operationListItem = ref<HTMLDivElement>(document.createElement("div"));
-
-onMounted(() => {
-  useSwipeOperation({ operationListItem });
-});
-
-const x = () => {
-  alert(`copying operation ${props.operation.operationID}`);
-};
 </script>
 
 <template>
   <NuxtLink :to="`/operations/${operation.operationID}`">
-    <div class="controls">
-      <button class="copy" @click.prevent.stop="x">
-        <svg class="bi" fill="currentColor" height="24" width="24">
-          <use xlink:href="bootstrap-icons/bootstrap-icons.svg#copy" />
-        </svg>
-      </button>
-    </div>
-
-    <div ref="operationListItem" class="operation">
+    <div class="operation">
       <div class="time">
         <div>
           {{ format(new Date(props.operation.at), "HH:mm") }}
@@ -66,20 +46,15 @@ const x = () => {
 
 <style scoped>
 a:has(.operation) {
-  position: relative;
   text-decoration: none;
 }
 
 .operation {
-  --translate: 0;
-
   background: hsla(0, 0%, 100%, 1);
   color: var(--bs-body-color);
   display: flex;
   gap: 1rem;
   padding: 0.5rem;
-  translate: var(--translate);
-  transition: translate 0.2s ease-in-out;
 }
 
 .operation:hover {
@@ -129,29 +104,5 @@ a:has(.operation) {
   color: gray;
   font-size: 0.9rem;
   text-align: right;
-}
-
-.controls {
-  align-items: center;
-  background: #333333;
-  color: whitesmoke;
-  display: flex;
-  gap: 1rem;
-  inset: 0;
-  justify-content: end;
-  position: absolute;
-}
-
-.copy {
-  background: transparent;
-  border: 0;
-  box-shadow: none;
-  color: inherit;
-  margin: 1rem;
-  padding: 0;
-}
-
-.copy:focus-visible {
-  outline-offset: 0.25rem;
 }
 </style>
